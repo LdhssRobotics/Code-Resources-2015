29468501b283cdd795611b7920a1d895533441f1
Survivor12 <ChrisChurchill27@gmail.com>
2015-02-11 17:02:37 -0500

    Update Robot.cpp

--- Camera Servo/src/Robot.cpp
+++ Camera Servo/src/Robot.cpp
@@ -10,13 +10,13 @@ mCamera(4)
 void OperatorControl()
 {
 while (IsOperatorControl() && IsEnabled()) {
-	float mCameraServoPosition=0.9;
+	float mCameraServoPosition=0.6;		//Starting postion
 	mCamera.Set(mCameraServoPosition);
 	if(stick.GetRawButton(3)) {
-	mCameraServoPosition+=0.0001;
+	mCameraServoPosition+=0.0001;		//Button X moves it down
 	}
 	else if(stick.GetRawButton(4)) {
-	mCameraServoPosition-=0.0001;
+	mCameraServoPosition-=0.0001;		//Button Y moves it up
 	}
 	Wait(0.0001);
   }
