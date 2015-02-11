c299561da88d27a75ac56662697e503ed3daa7e4
kieranbing <kbingham.ca@gmail.com>
2015-02-10 21:11:20 -0500

    Created first folder. Folder has working Ultrasonic code. Was finally able to figure it out. I'll show everyone in robotics on wednesday Feb 11.

---
+++ Ultrasonic/src/Robot.cpp
@@ -0,0 +1,37 @@
+#include "WPILib.h"
+
+class Robot : public SampleRobot {
+
+	Ultrasonic Sonic;
+
+	// update every 0.005 seconds/5 milliseconds.
+	double kUpdatePeriod = 0.005;
+
+public:
+	Robot() :
+			Sonic(3,4, Ultrasonic::kMilliMeters)
+	{
+	}
+
+	void OperatorControl() {
+
+		while (IsOperatorControl())
+		{
+			if (IsEnabled())
+			{
+				Sonic.SetAutomaticMode(true);
+				Sonic.SetEnabled(true);
+				double myDistance = (Sonic.GetRangeMM()/10);
+				SmartDashboard::PutNumber("Ultrasonic Measurement", myDistance);
+
+				Wait(kUpdatePeriod); // Wait 5ms for the next update.
+			}
+			else //Do when disabled
+			{
+				Sonic.SetAutomaticMode(false);
+			}
+		}
+	}
+};
+
+START_ROBOT_CLASS(Robot);
