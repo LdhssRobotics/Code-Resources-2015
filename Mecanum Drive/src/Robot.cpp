2f92b8a6791b3addb0e9c466c7ee73de70e460b7
Jacob <jakeincanada@icloud.com>
2015-02-11 18:06:07 -0500

    Update Robot.cpp
    applied more modern Mecanum code updates to this file and fixed some acceleration code

--- Mecanum Drive/src/Robot.cpp
+++ Mecanum Drive/src/Robot.cpp
@@ -6,59 +6,123 @@
 #define InvertForward 1 //-1 or 1, not for inverting actual wheels, just input axis
 #define InvertSide 1
 #define InvertRotate 1
-#define InvertFR 1 //-1 or 1, for inverting individual wheels
+#define InvertFR 1		//-1 or 1, for inverting individual wheels
 #define InvertBR 1
 #define InvertFL -1
 #define InvertBL -1
-/**
-* This is a change
-* This is a demo program showing the use of the RobotDrive class.
-* The SampleRobot class is the base of a robot application that will automatically call your
-* Autonomous and OperatorControl methods at the right time as controlled by the switches on
-* the driver station or the field controls.
-*
-* WARNING: While it may look like a good choice to use for your code if you're inexperienced,
-* don't. Unless you know what you are doing, complex code will be much more difficult under
-* this system. Use IterativeRobot or Command-Based instead if you're new.
-*/
-float CurSpeed[6] = { 0, 0, 0, 0, 0, 0 };
-class Robot: public SampleRobot {
-Joystick stick; // only joystick
-Talon motorFR;
-Talon motorBR;
-Talon motorFL;
-Talon motorBL;
-double kUpdatePeriod = 0.005;
-public:
-Robot() :
-stick(0), motorFR(0), motorBR(3), motorFL(1), motorBL(2),mCamera(4) {
-}
-float CurJoy[6] = { 0, 0, 0, 0, 0, 0 };
-float GetSmoothAxis(int axis) { //function for smoothing a target axis, in other words acceleration
-CurJoy[axis] = stick.GetRawAxis(axis); //fetch the target axis, since it is very important this is the same throught the math it is fetched once here
-if(!stick.GetRawButton(5)) {
-CurJoy[axis]=0;
-}
-if (true/*abs(CurJoy[axis]-CurSpeed[axis])>0.015*/) { //commented part would bypass changing the motor value if it was already close enough to the target value
-CurSpeed[axis] = CurSpeed[axis]+ ((abs(CurJoy[axis] - CurSpeed[axis]) / (CurJoy[axis] - CurSpeed[axis])) * 0.01); //bring the motor value closer to the joystick by 1%
-}
-// CurSpeed[axis] = CurJoy[axis]; //uncomment this if the robot does not accelerate or move, it will disable acceleration
-return CurJoy[axis]; //return the value for the smoothed axis
-}
-/**
-* Runs the motors with arcade steering.
-*/
-void OperatorControl()
+#define AccelVal 0.01f  // Acceleration step value: bring the motor value closer to the joystick by 1%
+
+class Robot: public IterativeRobot
 {
-while (IsOperatorControl() && IsEnabled()) {
-//uncomment this for full mecanum drive, disabled for acceleration testing
-motorFR.Set(InvertFR*(GetSmoothAxis(ForwardAxis)+GetSmoothAxis(RotateAxis)+GetSmoothAxis(TranslateAxis)));
-motorBR.Set(InvertBR*(GetSmoothAxis(ForwardAxis)+GetSmoothAxis(RotateAxis)-GetSmoothAxis(TranslateAxis)));
-motorFL.Set(InvertFL*(GetSmoothAxis(ForwardAxis)-GetSmoothAxis(RotateAxis)-GetSmoothAxis(TranslateAxis)));
-motorBL.Set(InvertBL*(GetSmoothAxis(ForwardAxis)-GetSmoothAxis(RotateAxis)+GetSmoothAxis(TranslateAxis)));
-//motorFR.Set(GetSmoothAxis(ForwardAxis)); //for testing we only turn one motor
-Wait(kUpdatePeriod); // wait for a motor update time
-}
-}
+	Joystick stick; // only joystick
+	Talon motorFR;
+	Talon motorFL;
+	Talon motorBL;
+	Talon motorBR;
+	Servo mCamera;
+	Encoder encoder;
+	Ultrasonic Sonic;
+	DigitalInput SonicPulse;
+	DigitalInput limitswitch;
+	LiveWindow *lw;
+	int autoLoopCounter;
+	double kUpdatePeriod = 0.005;
+	bool SmoothAccel = true;
+	float mCameraServoPosition = 0.5;         ///< Jacob - please confirm
+	float CurSpeed[6] = { 0, 0, 0, 0, 0, 0 }; ///< Current speed values over 6 axes
+	float CurJoy[6] = { 0, 0, 0, 0, 0, 0 };   ///< Current joystick values over 6 axes
+
+public:
+	Robot() :
+		stick(0),
+		motorFR(0),
+		motorFL(1),
+		motorBL(2),
+		motorBR(3),
+		mCamera(4),
+		encoder(1, 2, true),
+		Sonic(3,3, Ultrasonic::kMilliMeters),
+		SonicPulse(3),//To measure pulses manually (TEST)
+		limitswitch(0),
+		lw(NULL),
+		autoLoopCounter(0)
+	{
+	}
+
+	/**
+	 * Function for smoothing a target axis (in other words, applying acceleration).
+	 */
+	float GetSmoothAxis(int axis) {
+			//fetch the target axis, since it is very important this is the same throughout the math it is fetched once here
+			CurJoy[axis] = (true/*replace true with false to disable eg dead switch*/) ? stick.GetRawAxis(axis) : 0;
+			if (SmoothAccel) {
+				CurSpeed[axis] += (CurJoy[axis] > CurSpeed[axis]) ? AccelVal : -1*AccelVal; //bring the motor value closer to the joystick by 1%
+			}
+			else {
+				CurSpeed[axis] = CurJoy[axis];
+			}
+			return CurSpeed[axis]; //return the value for the smoothed axis
+		}
+
+private:
+	void RobotInit()
+	{
+		lw = LiveWindow::GetInstance();
+	}
+
+	void AutonomousInit()
+	{
+		autoLoopCounter = 0;
+	}
+
+	void AutonomousPeriodic()
+	{
+		if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
+		{
+
+			autoLoopCounter++;
+			} else {
+
+		}
+	}
+
+	void TeleopInit()
+	{
+
+	}
+
+	void TeleopPeriodic()
+	{
+		if (IsEnabled())
+		{
+			if(stick.GetRawAxis(2)>0.5) {
+				SmoothAccel = true;
+			}
+			else if(stick.GetRawAxis(3)>0.5) {
+				SmoothAccel = false;
+			}
+
+			//mecanum drive code calculates values for each motor and writes them
+			motorFR.Set(InvertFR*(GetSmoothAxis(ForwardAxis)+GetSmoothAxis(RotateAxis)+GetSmoothAxis(TranslateAxis)));
+			motorFL.Set(InvertFL*(GetSmoothAxis(ForwardAxis)-GetSmoothAxis(RotateAxis)-GetSmoothAxis(TranslateAxis)));
+			motorBL.Set(InvertBL*(GetSmoothAxis(ForwardAxis)-GetSmoothAxis(RotateAxis)+GetSmoothAxis(TranslateAxis)));
+			motorBR.Set(InvertBR*(GetSmoothAxis(ForwardAxis)+GetSmoothAxis(RotateAxis)-GetSmoothAxis(TranslateAxis)));
+
+			Wait(kUpdatePeriod);				// wait for a motor update tim
+		}
+		else //Do when disabled
+		{
+			SmartDashboard::PutNumber("is on", 0);
+			for(int i=0; i<5; i++) {
+			  CurSpeed[i] = 0;
+			}
+		}
+	}
+
+	void TestPeriodic()
+	{
+		lw->Run();
+	}
 };
+
 START_ROBOT_CLASS(Robot);
