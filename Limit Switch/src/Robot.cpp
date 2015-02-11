77c010919171155468476cd4e4442b2693a63425
kieranbing <kbingham.ca@gmail.com>
2015-02-10 22:09:30 -0500

    Added Limit Switch code

---
+++ Limit Switch/src/Robot.cpp
@@ -0,0 +1,33 @@
+#include "WPILib.h"
+
+class Robot : public SampleRobot {
+	DigitalInput limitswitch;
+	// update every 0.005 seconds/5 milliseconds.
+	double kUpdatePeriod = 0.005;
+
+public:
+	Robot() :
+			limitswitch(0)
+	{
+	}
+
+	void OperatorControl() {
+		while (IsOperatorControl())
+		{
+			if (IsEnabled())
+			{
+				if(limitswitch.Get() == 1)
+				{
+					SmartDashboard::PutString("LimitSwitch", "Pushed");
+				}else
+				{
+					SmartDashboard::PutString("LimitSwitch", "Not pushed");
+				}
+
+				Wait(kUpdatePeriod); // Wait 5ms for the next update.
+			}
+		}
+	}
+};
+
+START_ROBOT_CLASS(Robot);
