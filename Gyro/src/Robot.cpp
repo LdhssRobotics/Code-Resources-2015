101096891c361c6679b0cd7fe39a8a7306ffccaa
programmer <programmer@programmer-Latitude-E6410>
2015-03-06 17:07:57 -0500

    Added ability to print relative angles to the SmartDashboard.

--- Gyro/src/Robot.cpp
+++ Gyro/src/Robot.cpp
@@ -5,11 +5,17 @@ class Robot : public SampleRobot {
 	Gyro m_gyro;
 	RobotDrive m_drive;
 
+	float m_angle = 0.0;
+	float m_referenceAngle = 0.0;
+	float m_angleDelta = 0.0;
+	bool m_isAbsoluteAngle = false;
+
 	const uint32_t kFrontRightCh = 0; ///< Channel number for front right motor
 	const uint32_t kFrontLeftCh = 2; ///< Channel number for front left motor
 	const uint32_t kBackLeftCh = 3; ///< Channel number for back left motor
 	const uint32_t kBackRightCh = 1; ///< Channel number for back right motor
-	const double kUpdatePeriod = 0.005;
+	const float k_speedLimit = 0.25;
+	const double k_updatePeriod = 0.005;
 
 public:
 	Robot() :
@@ -23,11 +29,35 @@ public:
 	 * Prints Gyro readings as user controls robot's rotation.
 	 */
 	void OperatorControl() {
-		while (IsOperatorControl() && IsEnabled()) {
-			m_drive.MecanumDrive_Polar(0, 0, 0.25 * m_stick.GetAxis(4)); ///< Robot rotates using Joystick input.
+		float currentAngle = 0.0;
 
-			SmartDashboard::PutNumber("Current Angle: ", m_gyro.GetAngle()) ///< Gyro readings are printed to the SmartDashboard.
-			Wait(kUpdatePeriod); ///< Wait 5ms for the next update.
+		while (IsOperatorControl() && IsEnabled()) {
+			currentAngle = m_gyro.GetAngle();
+
+			m_drive.MecanumDrive_Polar(0, 0, k_speedLimit * m_stick.GetAxis(1)); ///< Robot rotates using Joystick input.
+
+			/**
+			 * If button pressed, toggles between printing absolute angle and relative angle.
+			 * Will set a reference point if set to printing relative angle.
+			 */
+			if(m_stick.GetRawButton(1))	{
+				m_isAbsoluteAngle = !m_isAbsoluteAngle;
+				if(!m_isAbsoluteAngle)	m_referenceAngle = currentAngle;
+			}
+
+			/**
+			 * Calculates the angle value to be printed.
+			 */
+			if(m_isAbsoluteAngle) {
+				m_angle = currentAngle;
+			} else {
+				m_angle = currentAngle - m_referenceAngle;
+				SmartDashboard::PutNumber("Reference Angle: ", m_referenceAngle);
+			}
+
+			SmartDashboard::PutNumber("Current Angle: ", m_angle) ///< Gyro readings are printed to the SmartDashboard.
+
+			Wait(k_updatePeriod); ///< Wait 5ms for the next update.
 		}
 	}
 };
